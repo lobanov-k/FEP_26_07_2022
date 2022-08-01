f8d70a17069b8fb228df28bde3dda965d5f42e48
Konstantin Lobanov <kostiantyn.lobanov@auto1.com>
2022-08-01 20:17:59 +0200

    2 lesson 29.07.2022

---
+++ 2_29_07/index.js
@@ -0,0 +1,163 @@
+// console.log(a);
+// myAdd();
+
+var a = "a";
+let b;
+// console.log(b);
+// console.log(a);
+
+// function myAdd() {
+//   // let myNum;
+//   console.log(a);
+// }
+// const a = () => {}
+
+const string = (str1 = "string");
+const boolean = true;
+const nullVar = null;
+const undefinedVar = undefined;
+
+// const obj = obj1 = {};
+
+let integer = 42;
+let float = 42.03;
+
+// console.log(1 / 0);
+
+// console.log(2 * "str");
+
+const bigIntVar = 12345555555555555555555555n;
+
+const articleText = "";
+
+// СТРОКИ
+const string1 = "string1";
+const string2 = "string1";
+const string3 = `string3 ${string2}`;
+
+const string4 = 'str "A"';
+const string5 = "str`` 'A'";
+const string6 = "str \"A \n B";
+
+// console.log(string3);
+// console.log(string6);
+
+const string7 = `
+1 line
+2line
+`;
+// console.log(string7);
+
+// console.log(typeof 1 == 'number');
+// console.log(typeof string1);
+// console.log(typeof true);
+
+// console.log(typeof []);
+
+const numVar = '42';
+console.log(typeof String(numVar)) ;
+
+const numStr = '400';
+// console.log(Number(numStr));
+
+// console.log('5' / '2');
+
+const numStr2 = '400text';
+// console.log(Number(numStr2));
+// console.log(parseInt(numStr2));
+
+const numStr3 = '400.4str20';
+
+const floatStr = '4meters';
+// console.log(parseFloat(floatStr));
+
+// console.log(parseInt(numStr3));
+
+// console.log(2 + 2);
+
+// console.log(+'2' + 2);
+// =
+
+console.log('John' + 'Doe');
+
+console.log(Number('2') + 2);
+
+const allSugar = '25';
+const price = '5';
+
+console.log(allSugar / price);
+
+console.log(typeof String(333));
+// = 
+console.log(typeof '' + 333);
+// '' + '333' = '333'
+
+console.log(2 + 2 + '2');
+console.log('2' + 2 + 2);
+
+console.log(+true);
+console.log(+false);
+console.log(+null);
+console.log(+undefined);
+console.log(+'');
+console.log(-'');
+
+const str4 = '';
+console.log(+str4);
+
+console.log(typeof null);
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean('0'));
+// console.log(Boolean(' '));
+// console.log(Boolean(-5));
+
+const minusVar = -5;
+console.log(-minusVar)
+
+let num5 = 1;
+// console.log(++num5);
+// console.log(num5++);
+// console.log(num5);
+// console.log(--num5);
+// console.log(num5--);
+// console.log(num5);
+
+const num6 = 9;
+console.log(num6%4);
+
+console.log((num6 - num6%4) / 4);
+
+console.log(2**3);
+
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 <= 2);
+console.log(1 >= 1);
+
+console.log('2' == 2); // true
+
+console.log('2' === 2); // false
+
+console.log('a' > 'A');
+console.log('adobe' > 'adobE'); // true
+console.log('adobe' == 'adobE'); // true
+
+console.log(1 != 2); // true
+console.log(1 != 1); // false
+
+console.log('LOGIC');
+
+console.log(!true);
+console.log(!false);
+console.log(!!0);
+// = Boolean(0)
+
+// alert('Advertisement');
+// const name1 = prompt('What is your name?');
+// console.log(name1);
+
+const value = confirm('Yes or no?');
+console.log(value);
