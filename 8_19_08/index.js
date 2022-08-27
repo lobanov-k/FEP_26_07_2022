d2d869492f224a21c9051b7256899e38b99ed4b7
Vadik529 <vadim.kobalt.vereshchak.9932@gmail.com>
2022-08-27 12:38:04 +0300

    changed constructor

--- 8_19_08/index.js
+++ 8_19_08/index.js
@@ -1,5 +1,8 @@
 const userName = prompt('Введите ваше имя');
-const userAge = prompt('Введите ваш возраст');
+
+
+
+const userAge = getUserAge();
 
 if (userAge < 18) {
     alert('Ошибка! Несовершеннолетние лица не могут быть владельцами автомобиля')
@@ -8,34 +11,43 @@ if (userAge < 18) {
     const carModel = prompt('Введите модель автомобиля');
     const carProductionYear = prompt('Введите год производства автомобиля');
     const carCountry = prompt('Введите страну производства автомобиля');
-
-    function checkEmptyString() {
-        if (this.name == '' || this.age == '' ||
-        this.brand == '' || this.model == '' ||
-        this.productionYear == '' || this.country == '') {
-          return alert(`Вы ввели не все значения. Проверьте правильность ввода и повторите попытку`) 
-        }
-    }
     
-    function User(name, age) {
-        this.name = name;
-        this.age = age;
-    }
     const newUser = new User(userName, userAge);
 
     checkEmptyString.apply(newUser);
-
-    function CarInfo(brand, model, productionYear, country, user) {
-        this.brand = brand;
-        this.model = model;
-        this.productionYear = productionYear;
-        this.country = country;
-        this.owner = user;
-    }
 
     const newCarInfo = new CarInfo(carBrand, carModel, carProductionYear, carCountry, newUser);
 
     checkEmptyString.apply(newCarInfo);
     
     console.log(newCarInfo);
+}
+
+function getUserAge() {
+    let age = +prompt('Введите ваш возраст');
+    if (isNaN(age)) {
+        return getUserAge()
+    } else {
+        return age
+    }
+}
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
+function CarInfo(brand, model, productionYear, country, user) {
+    this.brand = brand;
+    this.model = model;
+    this.productionYear = productionYear;
+    this.country = country;
+    this.owner = user;
+}
+
+function checkEmptyString() {
+    if (this.name == '' || this.age == '' ||
+    this.brand == '' || this.model == '' ||
+    this.productionYear == '' || this.country == '') {
+        return alert(`Вы ввели не все значения. Проверьте правильность ввода и повторите попытку`) 
+    }
 }
