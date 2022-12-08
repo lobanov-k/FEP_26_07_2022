a01e722bd1037d126ad74fae4d21ef1aa2b1fd19
smerch88 <maxarmyk@gmail.com>
2022-12-08 17:39:47 +0200

    hw 25 Arsenii

--- 28_8_11/public/app.js
+++ 28_8_11/public/app.js
@@ -8,7 +8,7 @@ const addUsersHtml = ({ id, name, age }) => {
 
   div.insertAdjacentHTML(
     "beforeend",
-    `<p><span>User: ${name}; Age: ${age}</span><button data-action="delete">Delete</button></p>`
+    `<p><span>User: ${name}; Age: ${age}</span><button data-action="delete">Delete</button><button data-action="edit" data-id="${id}" data-name="${name}" data-age="${age}">Edit</button></p>`
   );
 
   usersBlock.append(div);
@@ -57,5 +57,44 @@ usersBlock.addEventListener("click", function (event) {
   })
     .then((response) => response.json())
     .then(() => getUsers())
+    .catch((error) => alert(error.message));
+});
+
+usersBlock.addEventListener("click", function (event) {
+  if (event.target.dataset.action !== "edit") return;
+  const id = event.target.dataset.id;
+  const name = event.target.dataset.name;
+  const age = event.target.dataset.age;
+
+  const edit_form = document.forms.user_edit;
+  edit_form.querySelector('[name="id"]').value = id;
+  edit_form.querySelector('[name="name"]').value = name;
+  edit_form.querySelector('[name="age"]').value = age;
+  edit_form.style.display = "block";
+});
+
+document.forms.user_edit.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const id = this.id.value;
+  const name = this.name.value;
+  const age = this.age.value;
+
+  fetch(`${API_URL}/users/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ name, age }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      const edit_form = document.forms.user_edit;
+      edit_form.querySelector('[name="id"]').value = "";
+      edit_form.querySelector('[name="name"]').value = "";
+      edit_form.querySelector('[name="age"]').value = "";
+      edit_form.style.display = "none";
+
+      getUsers();
+    })
     .catch((error) => alert(error.message));
 });
